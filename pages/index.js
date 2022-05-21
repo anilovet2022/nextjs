25ee302cc2c2cae7e3a1bad2697a5f4d4fe0128a
anilovet2022 <anilovet20223@gmail.com>
2022-05-21 23:11:04 +0200

    get params

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export const getServerSideProps = (context) =>{
-  const refer = context.req.headers;
+  const refer = context.params;
   console.log(refer);
 
   // if(refer.includes('facebook')){
